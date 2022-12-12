a747cf2cd81d348be33def59312d483316e8e69b
Looney-James <jlooney88@nevada.unr.edu>
2022-12-12 02:10:13 -0800

    Hopefully this didn't mess up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MatDialogModule } from '@angular/material/dialog';
+import { GetstartedComponent } from './getstarted/getstarted.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    GetstartedComponent
   ],
   imports: [
     BrowserModule,
