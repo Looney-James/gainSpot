ad5a53e818a439f77a6b8f0ff3d9bbff92161284
Dustin Mader <dustinmader@me.com>
2022-12-11 23:59:36 -0800

    Added a login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MatDialogModule } from '@angular/material/dialog';
+// import { RouterModule } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import { MatDialogModule } from '@angular/material/dialog';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MatDialogModule
+    MatDialogModule,
+    // RouterModule.forRoot([
+    //   {
+    //     path: 'login',
+    //     component: LoginComponent
+    //   },
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
