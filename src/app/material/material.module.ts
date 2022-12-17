7c7bd67b602fb76e04f63e0e90b8b1c4ef44cccb
Looney-James <jlooney88@nevada.unr.edu>
2022-12-16 20:50:34 -0800

    added comments

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,3 +1,5 @@
+/*Angular material components*/
+
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
