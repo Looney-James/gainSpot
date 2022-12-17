87c2700f21c388246e95b26fb1cd13bd7028fb87
Dustin Mader <dustinmader@me.com>
2022-12-16 21:56:21 -0800

    Renamed button in dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     routingComponents,
-    LoginComponent
+    LoginComponent,
     GetstartedComponent,
     HomeComponent,
   ],
