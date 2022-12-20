9796bda9ab75d1612fea899f13093df318541821
2011-Bensadoun-Gerard <gerardbensa@nevada.unr.edu>
2022-12-19 23:23:29 -0800

    google api doesn't work yet

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
-  styleUrls: ['./map-page.component.css']
+  styleUrls: ['./map-page.component.scss']
 })
-export class MapPageComponent implements OnInit {
+export class HelloWorldComponent implements OnInit {
+
+
+  @ViewChild('googleMap', { static: true })
+
+  googleMapRef: ElementRef;
+
+
+
+  // Will contain the reference of rendered map instance 
+
+  map: google.maps.Map;
+
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.initMap();
+
   }
+
+
+  private initMap(): void {
+
+
+
+    const mapOptions: google.maps.MapOptions = {
+
+      // Centered to Hyderabad, India
+
+      center: new google.maps.LatLng(17.412127, 78.474921),
+
+      zoom: 15,
+
+    };
+
+
+
+    this.map = new google.maps.Map(this.googleMapRef.nativeElement, mapOptions);
+
+  }
+
 
 }
