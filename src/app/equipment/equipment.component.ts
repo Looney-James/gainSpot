e4bdc8a6a04a8824546d7aaaf5ecb8594ec30556
m8ttt <58528155+m8ttt@users.noreply.github.com>
2023-03-12 23:59:55 -0700

    updated report form UI, added option to select a gym, validators of inputs before submitting, report sucessfully sent message

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,44 +1,51 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { EquipmentService } from './equipment.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-equipment',
   templateUrl: './equipment.component.html',
   styleUrls: ['./equipment.component.css']
 })
-export class EquipmentComponent implements OnInit {
+export class EquipmentComponent {
+  @ViewChild('successModal') successModal: any; // add this line
 
-  public selectedEquipment = {
-    name: '',
-    status: '',
-    comments: ''
-  };
+  showSuccessMessage = false;
+  reportForm: FormGroup;
 
-  @ViewChild('reportModal') reportModal!: TemplateRef<any>;
-
-  constructor(
-    private equipmentService: EquipmentService,
-    private modalService: NgbModal,
-    private db: AngularFireDatabase
-  ) { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private db: AngularFireDatabase, public modalService: NgbModal) {
+    this.reportForm = this.formBuilder.group({
+      gym: ['', Validators.required],
+      name: ['', Validators.required],
+      status: ['', Validators.required],
+      comments: ['']
+    });
   }
 
-  openReportModal() {
-    this.modalService.open(this.reportModal);
-  }
-
-  submitReport() {
-    const report = {
-      name: this.selectedEquipment.name,
-      status: this.selectedEquipment.status,
-      comments: this.selectedEquipment.comments,
-      timestamp: Date.now()
-    };
-    this.db.list('/reports').push(report);
-    this.modalService.dismissAll();
-  }
+  submitReport() { 
+    if (this.reportForm.valid) {
+      const gym = this.reportForm.get('gym')?.value.replace(/\./g, '-');
+      if (gym) {
+        const equipment = this.reportForm.get('name')?.value;
+        const status = this.reportForm.get('status')?.value;
+        const comments = this.reportForm.get('comments')?.value;
+        const date = new Date().toISOString();
+        const report = { equipment, status, comments, date };
+  
+        this.db.list(`${gym}/reports`).push(report)
+          .then(() => {
+            console.log('Report submitted successfully');
+            this.reportForm.reset();
+            this.showSuccessMessage = true; // Set the flag to true
+            setTimeout(() => this.showSuccessMessage = false, 5000); // Hide the message after 5 seconds
+          })
+          .catch((error) => {
+            console.log('Error submitting report: ', error);
+          });
+      } else {
+        console.log('Gym is required.');
+      }
+    }
+  }  
 }
